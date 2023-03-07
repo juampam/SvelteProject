9569938886446fafc8cef662457b013815751f11
juampam <jpablomuralles@gmail.com>
2023-03-06 23:10:32 -0600

    Print data works

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+// @ts-nocheck
+
+    import "../tailwind.css"
+    import Nav from '$lib/Nav.svelte'
+    import Hero from '$lib/Hero.svelte'
+    import Table from "$lib/Table.svelte";
+    let show = false
+
+
+</script>
+
+<main data-theme="synthwave">
+    <div class="container mx-auto" >
+    <Nav />
+
+        <Table />
+
+    </div> 
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge : ['./src/**/*.svelte','./src/**/*.css'],
+  content: [],
+  theme: {  
+    daisyui: {
+      themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    },
+  },
+  plugins: [require("daisyui")],
+}
